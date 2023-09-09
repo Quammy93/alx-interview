5c45f297031e0877392d74de60abb31de50a4282
Quammy93 <Quammy93@gmail.com>
2023-09-09 08:31:22 -0700

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,55 +1,25 @@
-const fetch = require('node-fetch'); // Node.js environment
-
-function getCharacters(movieId) {
-  const baseUrl = 'https://swapi.dev/api/';
-  const filmEndpoint = `films/${movieId}/`;
-
-  return fetch(baseUrl + filmEndpoint)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Failed to retrieve film data');
-      }
-      return response.json();
-    })
-    .then((filmData) => {
-      return filmData.characters;
-    });
-}
-
-function printCharacters(movieId) {
-  getCharacters(movieId)
-    .then((charactersUrls) => {
-      const characterPromises = charactersUrls.map((characterUrl) =>
-        fetch(characterUrl).then((response) => {
-          if (!response.ok) {
-            throw new Error(`Failed to retrieve character data for ${characterUrl}`);
-          }
-          return response.json();
-        })
-      );
-
-      Promise.all(characterPromises)
-        .then((characters) => {
-          console.log(`Characters in Star Wars Episode ${movieId}:`);
-          characters.forEach((character) => {
-            console.log(character.name);
-          });
-        })
-        .catch((error) => {
-          console.error(error.message);
-        });
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
-}
-
+#!/usr/bin/node
+const request = require('request');
 const movieId = process.argv[2];
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-if (!movieId) {
-  console.error('Usage: node star_wars_characters.js <Movie ID>');
-  process.exit(1);
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
 }
-
-printCharacters(movieId);
-
